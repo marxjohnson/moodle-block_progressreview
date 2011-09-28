a614b4dbba3b5c599e537367146e432687448b79
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-09-28 14:14:04 +0100

    Tweaked link list to hide block entirely if no reviews are found

--- renderer.php
+++ renderer.php
@@ -5,13 +5,15 @@ class block_progressreview_renderer extends plugin_renderer_base {
     function review_list($courseswithreviews) {
         $output = '';
         foreach ($courseswithreviews as $session) {
-            $output .= $this->output->heading($session->name, 3);
-            $courselinks = array();
-            foreach ($session->courses as $course) {
-                $reviewurl = new moodle_url('/local/progressreview/subjectreview.php', array('sessionid' => $session->id, 'courseid' => $course->id));
-                $courselinks[] = html_writer::link($reviewurl, $course->fullname);
+            if (!empty($session->courses)) {
+                $output .= $this->output->heading($session->name, 3);
+                $courselinks = array();
+                foreach ($session->courses as $course) {
+                    $reviewurl = new moodle_url('/local/progressreview/subjectreview.php', array('sessionid' => $session->id, 'courseid' => $course->id));
+                    $courselinks[] = html_writer::link($reviewurl, $course->fullname);
+                }
+                $output .= html_writer::alist($courselinks);
             }
-            $output .= html_writer::alist($courselinks);
         }
         return $output;
     }
